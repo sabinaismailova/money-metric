f2ca5887eb962886d25d823e6c2f378b24c7b1f0
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2025-11-16 20:28:26 -0500

    Add responsiveness to chatbot related components components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Sidenavbar from "../components/navbars/sidenavbar";
 import Topnavbar from "../components/navbars/topnavbar";
