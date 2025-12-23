91f944dea31de5bc0a6e02eec66fa113b056d098
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2025-12-23 12:47:30 -0500

    Remove proxy to check root of login issue

--- proxy.js
+++ proxy.js
@@ -1,28 +1,28 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function proxy(req) {
-//   const { pathname } = req.nextUrl;
+export async function proxy(req) {
+  // const { pathname } = req.nextUrl;
 
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user`, {
-//     headers: {
-//       cookie: req.headers.get("cookie") || "",
-//     },
-//     credentials: "include",
-//   });
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user`, {
+  //   headers: {
+  //     cookie: req.headers.get("cookie") || "",
+  //   },
+  //   credentials: "include",
+  // });
 
-//   const isLoggedIn = res.ok;
+  // const isLoggedIn = res.ok;
 
-//   if (!isLoggedIn && pathname === "/dashboard") {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
-//   if (!isLoggedIn && pathname === "/addTransaction") {
-//     return NextResponse.redirect(new URL("/", req.url));
-//   }
-//   if (isLoggedIn && pathname === "/") {
-//     return NextResponse.redirect(new URL("/dashboard", req.url));
-//   }
-//   return NextResponse.next();
-// }
+  // if (!isLoggedIn && pathname === "/dashboard") {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
+  // if (!isLoggedIn && pathname === "/addTransaction") {
+  //   return NextResponse.redirect(new URL("/", req.url));
+  // }
+  // if (isLoggedIn && pathname === "/") {
+  //   return NextResponse.redirect(new URL("/dashboard", req.url));
+  // }
+  // return NextResponse.next();
+}
 
 // export const config = {
 //   matcher: ["/", "/dashboard", "/addTransaction"],
